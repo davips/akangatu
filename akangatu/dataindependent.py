9fe04da79bd6e5f2d51878a00e577c5217c692d4
davips <dpsabc@gmail.com>
2020-09-19 03:52:18 -0300

    Solve NoData dependence making akangatu dependent on aiuna

--- akangatu/dataindependent.py
+++ akangatu/dataindependent.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+from aiuna.content.specialdata import NoData
 from akangatu.mixin.operators import withOperators
 from akangatu.mixin.sampling import withSampling
 from transf.dataindependent import DataIndependent_
@@ -7,3 +8,6 @@ from transf.dataindependent import DataIndependent_
 
 class DataIndependent(DataIndependent_, withSampling, withOperators, ABC):
     """Operable and sampleable data transformer that doesn't dependent on (training) data."""
+
+    def _internal_transform_(self, data=None, lazy=False):  # override
+        return self._transform_(data or NoData)
