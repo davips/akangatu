e2cb17a7e388394141fc72de61e03d06d1775af6
davips <dpsabc@gmail.com>
2020-09-20 03:24:40 -0300

    Add globalcache for models

--- akangatu/operator/binary/chain.py
+++ akangatu/operator/binary/chain.py
@@ -1,26 +1,42 @@
 from functools import cached_property
 
+import ring as ring
+
+from transf.absdata import InnocuousInnerData
+from transf.operator import Mul
 from transf.transformer import Transformer
 
 
-class Chain(Transformer):
-    def __init__(self, transformers, **kwargs):
+class Chain(Mul, Transformer):
+    def _inner_(self):
+        return self._inner
+
+    def __init__(self, *transformers):
+        super().__init__(*transformers)
         self.transformers = transformers
         self._config = {"transformers": transformers}
+        self._inner = InnocuousInnerData()
 
-    def _transform_(self, data):
-        pass
+    def _core_transform_(self, data):  # TODO: expose internal models
+        for transf in self.transformers:
+            data = transf.transform(data)
+        return data
 
     def _config_(self):
         return self._config
 
     def sample(self, track=False):  # TODO: seed
-        return Chain(transf.sample(track) for transf in self.transformers)
+        return Chain(*[transf.sample(track) for transf in self.transformers])
 
     @cached_property
     def parameters(self):  # override
         # return Parameters(self.name, self.context, {"transformers": [transf.parameters for transf in self.transformers]})
         return {"transformers": [transf.parameters for transf in self.transformers]}
+
+    def __call__(self, inner):  # TODO: seed
+        instance = self.__class__(*self.transformers)
+        instance._inner = inner
+        return instance
 
 
 def traverse(params):
