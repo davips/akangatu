ffdc41916c0512784babb21d752b64140d20bf76
davips <dpsabc@gmail.com>
2020-09-24 01:28:09 -0300

    Handling of inner at init moved from DDT.new to Meta.new

--- akangatu/delimiter.py
+++ akangatu/delimiter.py
@@ -3,9 +3,9 @@ from akangatu.abs.marker import Marker
 
 class Begin(Marker):  # HINT: dataclasses does not work inside operate(): "object type has no isclass attribute"
     def _longname_(self):
-        return "{" + self._transfuuid
+        return "{-" + self._transfuuid
 
 
 class End(Marker):
     def _longname_(self):
-        return self._transfuuid + "}"
+        return self._transfuuid + "-}"
