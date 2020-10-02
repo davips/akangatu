5de16ad56c2820f2ea2db13664e6c36564660a22
davips <dpsabc@gmail.com>
2020-10-01 23:22:28 -0300

    Add Explain stub / add Margin Refix wrong idmat in marker

--- akangatu/abs/marker.py
+++ akangatu/abs/marker.py
@@ -1,8 +1,9 @@
 from akangatu.container import Container1
 from akangatu.rev import Rev
 from transf.absdata import AbsData
+from transf.mixin.noop import asNoOp
 
 
-class Marker(Container1):  # REMINDER: asNoOp aqui faria perder a identidade quando armazenado.
+class Marker(asNoOp, Container1):  # REMINDER: should be asNoOp since inside Data stay import UUIDs.
     def _process_(self, data: AbsData):
         return data.replace(self).replace(Rev(self))
