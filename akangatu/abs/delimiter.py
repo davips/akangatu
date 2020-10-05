1262e7d8026dfbd5409aed5588795e5bd9cd8d90
davips <dpsabc@gmail.com>
2020-10-05 20:52:46 -0300

    Workaround in workflow to avoid SVM bug Fix ambiguity for step in delimiter

--- akangatu/abs/delimiter.py
+++ akangatu/abs/delimiter.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from functools import cached_property
 
 from akangatu.container import Container1
@@ -11,7 +11,7 @@ class BadStep(Exception):
     """Ill-implemented Step"""
 
 
-class Delimiter(asNoOp, Container1):
+class Delimiter(asNoOp, Container1, ABC):
     """Only internal step and its reverse are visible
 
     step should be a marker (mixed in with asMarker).
@@ -25,26 +25,28 @@ class Delimiter(asNoOp, Container1):
     # SOLUTION: the implementer/user should Rev the marker
     # FINAL: we provide both visible and invisible versions of the marker
     def _process_(self, data: AbsData):
-        if not isinstance(self.step, asDelimiter):
+        from akangatu.marker import asMarker
+        if not issubclass(self.wrapper, asMarker):
             raise BadStep("Step for a reversed Marker should be mixed in with asMarker")
-        return (self.step * Rev(self.step)).process(data)
+        wrapped = self.wrapper(self.step)
+        return (wrapped * Rev(wrapped)).process(data)
 
     @cached_property
-    def step(self):
-        return self._step_()
+    def wrapper(self):
+        return self._wrapper_()
 
     @abstractmethod
-    def _step_(self):
+    def _wrapper_(self):
         pass
 
 
 class Begin(Delimiter):
-    def _step_(self):
+    def _wrapper_(self):
         from akangatu.marker import B
-        return B(self.step)
+        return B
 
 
 class End(Delimiter):
-    def _step_(self):
+    def _wrapper_(self):
         from akangatu.marker import E
-        return E(self.step)
+        return E
