ed7ecb20045365b5fc568d34901e215901f0e047
davips <dpsabc@gmail.com>
2020-10-09 03:08:09 -0300

    Update gitignore

--- noop.py
+++ noop.py
@@ -1,9 +1,9 @@
 from akangatu.distep import DIStep
 from transf.absdata import AbsData
-from transf.mixin.config import asUnitset
+from transf.mixin.config import asUnitset, asConfigLess
 from transf.mixin.noop import asNoOp
 
 
-class NoOp(asUnitset, DIStep, asNoOp):
+class NoOp(asConfigLess, asUnitset, DIStep, asNoOp):
     def _process_(self, data: AbsData):
         return data
