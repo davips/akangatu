d2a27c38d3fb54851a53fdcf3e434a092f6e2bc3
davips <dpsabc@gmail.com>
2020-10-17 20:31:00 -0300

    Trying to abolish unpicklable() / Picklable

--- noop.py
+++
@@ -1,9 +0,0 @@
-from akangatu.distep import DIStep
-from transf.absdata import AbsData
-from transf.mixin.config import asUnitset, asConfigLess
-from transf.mixin.noop import asNoOp
-
-
-class NoOp(asConfigLess, asUnitset, DIStep, asNoOp):
-    def _process_(self, data: AbsData):
-        return data
