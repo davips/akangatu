5a557c23de6de6fa5ff843833505715ff504530c
davips <davips@users.noreply.github.com>
2020-12-15 18:15:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 """Setup for akangatu package."""
 #  Copyright (c) 2020. Davi Pereira dos Santos
-#  This file is part of the autoreadme project.
+#  This file is part of the akangatu project.
 #  Please respect the license - more about this in the section (*) below.
 #
-#  autoreadme is free software: you can redistribute it and/or modify
+#  akangatu is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  autoreadme is distributed in the hope that it will be useful,
+#  akangatu is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with autoreadme.  If not, see <http://www.gnu.org/licenses/>.
+#  along with akangatu.  If not, see <http://www.gnu.org/licenses/>.
 #
 #  (*) Removing authorship by any means, e.g. by distribution of derived
 #  works or verbatim, obfuscated, compiled or rewritten versions of any
@@ -29,16 +29,16 @@ import akangatu
 NAME = "akangatu"
 
 
-VERSION = 0.1
+VERSION = "0.20.15-a"
 
 
 AUTHOR = 'Davi Pereira-Santos'
 
 
-AUTHOR_EMAIL = ''
+AUTHOR_EMAIL = 'dpsabc@gmail.com'
 
 
-DESCRIPTION = 'Science as data akangatuormation'
+DESCRIPTION = 'Data Handling and Science'
 
 
 with open('README.md', 'r') as fh:
@@ -59,7 +59,7 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Natural Language :: English',
                'Programming Language :: Python',
                'Topic :: Scientific/Engineering',
-               'Operating System :: Linux',
+#               'Operating System' :: 'POSIX' :: 'Linux',
                'Programming Language :: Python :: 3.8']
 
 
